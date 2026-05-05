d789a17a49b98b44d6c2b97973bcee213b2a5c8f
javier-gitc-code <jodg1987@gmail.com>
2026-05-04 22:46:53 -0500

    Agregando Clase Cliente con encapsulamiento

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ import datetime
 # 1. ESCUDO: Para que el programa no se cierre si hay errores
 class ErrorSoftwareFJ(Exception):
     pass
+from abc import ABC, abstractmethod
+
+# 1. CLASE CLIENTE (Con datos protegidos y validaciones robustas)
+class Cliente:
+    def __init__(self, nombre, cedula):
+        # Validación estricta según el Anexo 3
+        if not nombre.strip() or not cedula.strip():
+            raise ErrorSoftwareFJ("Datos de cliente incompletos.")
+        
+        self.__nombre = nombre  # Encapsulamiento (Dato privado)
+        self.__cedula = cedula  # Encapsulamiento (Dato privado)
+
+    def obtener_nombre(self):
+        return self.__nombre
 
 # 2. CHISMOSO: Para anotar los fallos en un bloc de notas
 def registrar_log(mensaje):
